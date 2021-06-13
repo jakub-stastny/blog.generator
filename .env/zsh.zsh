7170e4a264d63b6e48bf6d4c54eff3a2d9eb9763
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-13 00:42:12 +0000

    Basic infrastructure

---
+++ .env/zsh.zsh
@@ -0,0 +1,25 @@
+load ~/.zsh/environments/helpers.zsh && save-function-list
+load ~/.zsh/environments/basic.zsh
+load ~/.zsh/environments/emacs.zsh
+
+start-emacs-session
+rename-first-tab
+
+# Custom functions & aliases.
+
+# We compile on save, but for the first-time compilation,
+# we want to be able to do it from the shell, rather than
+# having to go and save manually each file from Emacs.
+compile() {
+  cd src
+  echo "(dolist (file argv) (message file) (find-file file) (org-babel-tangle))" > compile.el
+  emacs --script compile.el *.org
+  rm compile.el
+  cd -
+}
+
+generate-readme() {
+  ../blog.meta/bin/generate-readme $PWD
+}
+
+report-custom-functions
